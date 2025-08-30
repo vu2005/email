76719343559e8219aa9692d0ba13192050b31d20
vu2005 <vunnth2407024@fpt.edu.vn>
2025-08-30 18:15:36 +0700

    64

---
+++ footer/footer.js
@@ -0,0 +1,73 @@
+// footer.js
+const emailFooter = `
+  <table style="width:100%;text-align:center;border-collapse:separate">
+    <tbody>
+      <!-- Social Icons -->
+      <tr>
+        <td align="center" style="padding:10px 10px 20px">
+          <a href="" style="display:inline-block;margin:2px" target="_blank">
+            <img alt="Facebook" height="40" width="40"
+              src="https://ci3.googleusercontent.com/meips/ADKq_NZkfMVt-Hiz_LdWR6TufEnm9TsBCwNKpvI52dDI6aBTzHVFspd5R4VCFdW1q0yV9xYk7s2lKwCCABQ8kCb_1f2m8eTRbKf73Ha4WEPnHUs5qQ_NNVOMHlH8np32dbBirfBfo69r52GIeNOREL5rcurJ3JW2NnCi35hb5SxImiApbb_ReSs4pIgkEKAnZKJ6Lg=s0-d-e1-ft#https://appboy-images.com/appboy/communication/assets/image_assets/images/6033596e141af42e11eb9011/original.png?1613977966"
+              style="width:40px;height:40px">
+          </a>
+          <a href="" style="display:inline-block;margin:2px" target="_blank">
+            <img alt="Pinterest" height="40" width="40"
+              src="https://ci3.googleusercontent.com/meips/ADKq_NY40Jk9Q9r8EYNjNSqhxdyuHfYDoAUyypZ0ZQrrpvog4WHJCG1dnI7fqfm5GyCk3gKlCfPUIiIs6NwsN8unN1yNbEzBlP8SdHB5FelnTkPZ0JBAEwFtxsMYXefqzobP9tDwsIaTuE2vMXX4rxzb6l2OBewnQK4SH9WdKDkIW4vVQXIUyp1cKDUmQpMwfI4JIg=s0-d-e1-ft#https://appboy-images.com/appboy/communication/assets/image_assets/images/6033597389461f339542c0be/original.png?1613977971"
+              style="width:40px;height:40px">
+          </a>
+          <a href="" style="display:inline-block;margin:2px" target="_blank">
+            <img alt="Instagram" height="40" width="40"
+              src="https://ci3.googleusercontent.com/meips/ADKq_NYma5Mt_uO53kSV2TjrVGoU3s3Hj8ntUqCxzABv1hd9kqyiTV4KAd2ngNnsBmWsPGc8jjOKFBM_wguwT7Z_MFyy76dzJz9m8nsPYAjlZC70UkdKEXVoQszhC84e0e7Y7ZzADsO1bMusM658fHOMnpX9_hWgqwNR_7k7PAfdZ7JCpxr2YBQ1F_ZjqnnwEmhSvA=s0-d-e1-ft#https://appboy-images.com/appboy/communication/assets/image_assets/images/6033595124413112353135b6/original.png?1613977937"
+              style="width:40px;height:40px">
+          </a>
+        </td>
+      </tr>
+
+      <!-- Info + Links -->
+      <tr>
+        <td align="center"
+            style="color:#c5c6c9;font-size:12px;line-height:18px;
+                   font-family:'Plus Jakarta Sans',sans-serif;
+                   padding:0px 5px">
+          <span style="font-size:12px">
+            <span style="font-family:'Plus Jakarta Sans',sans-serif">
+              <a href="" style="color:black;text-decoration:none;font-weight:bold" target="_blank">Privacy Policy</a> | 
+              <a href="" style="color:black;text-decoration:none;font-weight:bold" target="_blank">Support</a> | 
+              <a href="https://strongbody.ai/affiliate/overview" style="color:black;text-decoration:none;font-weight:bold" target="_blank">Refer & Earn</a> | 
+              <a href="" style="color:black;text-decoration:none;font-weight:bold" target="_blank">Get Inspired</a>
+              <br><br>
+              <span style="color:#7a7d85">
+                This email was sent to you by StrongBody SG PTE, LTD.<br>
+                You are receiving this email because you signed up for StrongBody.ai<br>
+                If you wish to unsubscribe from all future emails, please click  
+                <a href="https://strongbody.ai/?ref=stronghealth" style="color:black;text-decoration:none" target="_blank">here</a>.
+                <br><br>
+                105 CECIL STREET #18-20 THE OCTAGON Singapore, 069534 Singapore <br>
+                StrongBody wellness marketplace, now in Multime, your trusted voice social app for connections
+              </span>
+            </span>
+          </span>
+        </td>
+      </tr>
+
+      <!-- App Badges -->
+      <tr>
+        <td align="center" style="padding:20px 10px">
+          <a href="" style="display:inline-block;width:120px;height:36px;margin:4px;padding-right:15px;" target="_blank">
+            <img alt="Multime" height="36"
+              src="https://storage.googleapis.com/strongbody/upload-manually/multime.jpg"
+              width="120">
+          </a>
+          <a href="" style="display:inline-block;margin:5px" target="_blank">
+            <img src="https://ci3.googleusercontent.com/meips/ADKq_NZWl7EIjscaUbeUcbgKpvPwgEjIuXlLnwgesEPmEl6Xdag5F98eCpacO6Xzwvc_LYpd3tk19eCItP3K1aJl6UUY7cVoLdIconuyThElGiyHj0Njj21gqEundUrsbbSQLYwOkWj-NnXXqZEd12h_okqN2k_VTNrDzkm4driCkBshS_-YVCuEoNzwYdVIPenEWQ=s0-d-e1-ft#https://appboy-images.com/appboy/communication/assets/image_assets/images/60335ea2798dc82632426968/original.png?1613979298"
+              width="120">
+          </a>
+          <a href="" style="display:inline-block;margin:5px" target="_blank">
+            <img src="https://ci3.googleusercontent.com/meips/ADKq_Na5AnnJzd14X47VOF7R-WTkbmEgzKJOxuh42XqiISpm6ndZ5tT_JGonezLr7mppPJ6siM9Cd_pXLjcQhRQp3T6maALt-hOipMyy5HMakhWT5Xh97hpWmn2UkvrY0gb5cJzywPz8Fa0dTe0vyQ50XqcaAcGopTgS4h9fqiJAFSayGmAyKPc_gtMv0uDc2awoWQ=s0-d-e1-ft#https://appboy-images.com/appboy/communication/assets/image_assets/images/60335eaf83566879cab564c3/original.png?1613979311"
+              width="120">
+          </a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+`;
